14acc43ece831a3a499e2d1a6e9d0765b2e97e68
Maurice O'Connor <maurice.oconnor@gmail.com>
2017-06-24 16:53:31 +0100

    add stuff

--- flac2alac.sh
+++ flac2alac.sh
@@ -3,7 +3,7 @@
 set -e
 shopt -s globstar
 
-echo "=== Converting flacs `date` ===" \
+echo "=== Converting flacs in `pwd` on `date` ===" \
         && for f in ./**/*.flac; do avconv -n -i "$f" -c:a alac "${f%.*}.m4a" && rm "$f" |& tee -a flac2alac.log; done
 
 

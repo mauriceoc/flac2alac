f8165fb812d1612fb565448da22c45e513e50737
Maurice O'Connor <maurice.oconnor@gmail.com>
2017-06-24 17:03:28 +0100

    new stuff

--- flac2alac.sh
+++ flac2alac.sh
@@ -1,10 +1,12 @@
 #!/usr/local/bin/bash
 
+exec 1> >(tee -a flac2alac.log) 2>&1
+
 set -e
-shopt -s globstar
+shopt -s globstar # must have right version of bash for this
 
 echo "=== Converting flacs in `pwd` on `date` ===" \
-        && for f in ./**/*.flac; do avconv -n -i "$f" -c:a alac "${f%.*}.m4a" && rm "$f" |& tee -a flac2alac.log; done
+        && for f in ./**/*.flac; do avconv -n -i "$f" -c:a alac "${f%.*}.m4a" && rm "$f"; done
 
 if [ "$?" -eq 0 ]; then
         echo "Success!"

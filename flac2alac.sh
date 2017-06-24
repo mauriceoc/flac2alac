4bbb81a458d8a2689daa1a16cb625cc369cd35da
Maurice O'Connor <maurice.oconnor@gmail.com>
2017-06-24 16:55:07 +0100

    add message

--- flac2alac.sh
+++ flac2alac.sh
@@ -6,4 +6,9 @@ shopt -s globstar
 echo "=== Converting flacs in `pwd` on `date` ===" \
         && for f in ./**/*.flac; do avconv -n -i "$f" -c:a alac "${f%.*}.m4a" && rm "$f" |& tee -a flac2alac.log; done
 
+if [ "$?" -eq 0 ]; then
+        echo "Success!"
+else
+        echo "Failure!"
+fi
 
